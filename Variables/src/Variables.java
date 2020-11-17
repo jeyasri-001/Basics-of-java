1bb3e846fdbd034ce8244313b3edd4b5feec56dd
jeyasri__001 <56224991+jeyasri-001@users.noreply.github.com>
2020-11-17 21:29:05 +0530

    Add files via upload

---
+++ Variables/src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args){
+        int currentScore = 10;
+        int wrongAns = 3;
+        int rightAns =7;
+        int finalScore1 = currentScore-wrongAns;
+        int finalScore2 = currentScore*rightAns;
+
+        System.out.println(finalScore1);
+        System.out.println(finalScore2);
+    }
+}
